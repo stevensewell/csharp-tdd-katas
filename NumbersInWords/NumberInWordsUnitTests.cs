9bb10a7c6e6ceb95f81bdcf936164dd880bf8165
Steven Sewell <steven.sewell@krs.co.za>
2022-11-16 09:51:57 +0200

    Failing ShouldConvert20UpTo90IncrementsOf10

--- NumbersInWords/NumberInWordsUnitTests.cs
+++ NumbersInWords/NumberInWordsUnitTests.cs
@@ -34,4 +34,19 @@ public class NumberInWordsUnitTests
         var (_, numberInWords) = new NumberConverter(number);
         Assert.That(numberInWords, Is.EqualTo(expectedNumberInWords));
     }
+    
+    [Test]
+    [TestCase(20, "twenty")]
+    [TestCase(30, "thirty")]
+    [TestCase(40, "forty")]
+    [TestCase(50, "fifty")]
+    [TestCase(60, "sixty")]
+    [TestCase(70, "seventy")]
+    [TestCase(80, "eighty")]
+    [TestCase(90, "ninety")]
+    public void ShouldConvert20UpTo90IncrementsOf10(int number, string expectedNumberInWords)
+    {
+        var (_, numberInWords) = new NumberConverter(number);
+        Assert.That(numberInWords, Is.EqualTo(expectedNumberInWords));
+    }
 }
