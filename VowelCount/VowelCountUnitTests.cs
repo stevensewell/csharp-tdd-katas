7953e29b9dfcee84f2bc31b6aaec4e3b8ab0cdfb
Steven Sewell <steven.sewell@krs.co.za>
2022-11-17 11:32:32 +0200

    Failing ShouldCalculateNumberOfVowels

--- VowelCount/VowelCountUnitTests.cs
+++ VowelCount/VowelCountUnitTests.cs
@@ -2,5 +2,15 @@ namespace VowelCount;
 
 public class Tests
 {
- 
+    [Test]
+    [TestCase("abracadabra", 5)]
+    [TestCase("pear tree", 4)]
+    [TestCase("o a e asd rfbwe", 5)]
+    [TestCase("aeiou", 5)]
+    [TestCase("aypyx", 0)]
+    public void ShouldCalculateNumberOfVowels(string input, int expected)
+    {
+        var actual = VowelCount.Calculate(input);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 }
