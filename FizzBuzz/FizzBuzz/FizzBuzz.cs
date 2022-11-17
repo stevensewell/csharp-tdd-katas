b5b925de18b53a456c38f12676ce4fdd482c6289
Steven Sewell <steven.sewell@krs.co.za>
2022-11-17 10:02:15 +0200

    Failing DivisibleByThree

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,14 @@
+﻿namespace FizzBuzz;
+
+public static class FizzBuzz
+{
+    public static IList<string> Calculate(int count)
+    {
+    }
+    
+    [Test]
+    public static void DivisibleByThree()
+    {
+        Assert.That(DetermineFizzBuzz(3), Is.EqualTo("Fizz"));
+    }
+}
---
+++ FizzBuzz/FizzBuzzUnitTests.cs
@@ -0,0 +1,6 @@
+namespace FizzBuzz;
+
+public class Tests
+{
+
+}
