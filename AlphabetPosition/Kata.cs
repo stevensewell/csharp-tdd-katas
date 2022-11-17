db0fe270579973b1f70704b21f7336be982c40c1
Steven Sewell <steven.sewell@krs.co.za>
2022-11-17 12:29:58 +0200

    Alphabet position

---
+++ AlphabetPosition/Kata.cs
@@ -0,0 +1,16 @@
+﻿namespace AlphabetPosition;
+
+public class Kata
+{
+  public static object? AlphabetPosition(string input)
+  {
+    return input.Select(CalculatePosition).Aggregate((acc, next) => string.IsNullOrWhiteSpace(next) ? acc : $"{acc} {next}");
+  }
+
+  private static string CalculatePosition(char arg)
+  {
+    return !char.IsLetter(arg) 
+      ? string.Empty 
+      : (char.ToUpper(arg) - 64).ToString();
+  }
+}
