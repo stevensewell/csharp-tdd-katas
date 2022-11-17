7ce5f98bd480b2a96d7eccc072c3b721a4117cd0
Steven Sewell <steven.sewell@krs.co.za>
2022-11-17 11:02:51 +0200

    Passing ShouldAlternateCase

---
+++ AlternatingCase/Extensions/StringExt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Extensions
+{
+    public static class StringExt
+    {
+        public static string ToAlternatingCase (this string s)
+        {
+            return s
+                .ToCharArray()
+                .Select(AlternateCase)
+                .Aggregate((x, y) => x + y);
+        }
+
+        private static string AlternateCase(char chr)
+        {
+            return chr switch
+            {
+                _ when char.IsUpper(chr) => char.ToLower(chr).ToString(),
+                _ when char.IsLower(chr) => char.ToUpper(chr).ToString(),
+                _ => chr.ToString()
+            };
+        }
+    }
+}
