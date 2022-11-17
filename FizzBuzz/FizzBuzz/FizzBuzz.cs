59a82dd49c7ae9f0117b1a0041145241e9f333df
Steven Sewell <steven.sewell@krs.co.za>
2022-11-17 10:02:40 +0200

    Passing DivisibleByThree

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -4,6 +4,17 @@ public static class FizzBuzz
 {
     public static IList<string> Calculate(int count)
     {
+      return Enumerable.Range(1, count)
+        .Select(DetermineFizzBuzz)
+        .ToList();
+    }
+
+    private static string DetermineFizzBuzz(int number)
+    {
+        return number switch
+        {
+            _ when number % 3 == 0 => "Fizz",
+        };
     }
     
     [Test]
