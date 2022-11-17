d2728d14db0f8f09b5adf81bfc046cb186f2a07e
Steven Sewell <steven.sewell@krs.co.za>
2022-11-17 10:19:50 +0200

    Failing ShouldCalculateUpTo100

--- FizzBuzz/FizzBuzzUnitTests.cs
+++ FizzBuzz/FizzBuzzUnitTests.cs
@@ -1,6 +1,11 @@
 namespace FizzBuzz;
 
-public class Tests
+public partial class FizzBuzzUnitTests
 {
-
+    [Test]
+    public void ShouldCalculateUpTo100()
+    {
+        var result = FizzBuzz.Calculate(100);
+        Assert.That(result, Is.EqualTo(_fizzBuzzExpectedResultUpTo100));
+    }
 }
